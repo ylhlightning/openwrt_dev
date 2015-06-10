567d82028e785f512c90852235312e3978d60d8d
linhu <linhu.ying@u-blox.com>
2015-06-10 10:38:35 +0200

    fix the message corruption problem.

--- package/ublox/app/ublxaqappserver/src/ublx_aqapp_wwan_api.c
+++ package/ublox/app/ublxaqappserver/src/ublx_aqapp_wwan_api.c
@@ -306,13 +306,17 @@ static int wwan_if_enable(struct ubus_context *ctx, struct ubus_object *obj,
   const char *format = "%s received a message: %s";
   char data[1024];
   char *msgstr = data;
+  int hreq_size;
 
   blobmsg_parse(wwan_if_enable_policy, ARRAY_SIZE(wwan_if_enable_policy), tb, blob_data(msg), blob_len(msg));
 
   if (tb[WWAN_IF_ENABLE_MSG])
     msgstr = blobmsg_data(tb[WWAN_IF_ENABLE_MSG]);
 
-  hreq = calloc(1, sizeof(*hreq) + strlen(format) + strlen(obj->name) + strlen(msgstr) + 1);
+  hreq_size = sizeof(*hreq) + strlen(format) + strlen(obj->name) + strlen(msgstr) + 1;
+  hreq = calloc(1, hreq_size);
+  memset(hreq, 0, hreq_size);
+  memset(msgstr, 0, 1024);
 
   if(wwan_if_enable_do(msgstr) == FALSE)
   {
@@ -418,13 +422,17 @@ static int wwan_if_disable(struct ubus_context *ctx, struct ubus_object *obj,
   const char *format = "%s received a message: %s";
   char data[1024];
   char *msgstr = data;
+  int hreq_size;
 
   blobmsg_parse(wwan_if_disable_policy, ARRAY_SIZE(wwan_if_disable_policy), tb, blob_data(msg), blob_len(msg));
 
   if (tb[WWAN_IF_DISABLE_MSG])
     msgstr = blobmsg_data(tb[WWAN_IF_DISABLE_MSG]);
 
-  hreq = calloc(1, sizeof(*hreq) + strlen(format) + strlen(obj->name) + strlen(msgstr) + 1);
+  hreq_size = sizeof(*hreq) + strlen(format) + strlen(obj->name) + strlen(msgstr) + 1;
+  hreq = calloc(1, hreq_size);
+  memset(hreq, 0, hreq_size);
+  memset(msgstr, 0, 1024);
 
   if(wwan_if_disable_do(msgstr) == FALSE)
   {
@@ -548,13 +556,17 @@ static int wwan_if_connect(struct ubus_context *ctx, struct ubus_object *obj,
   const char *format = "%s received a message: %s";
   char data[1024];
   char *msgstr = data;
+  int hreq_size;
 
   blobmsg_parse(wwan_if_connect_policy, ARRAY_SIZE(wwan_if_connect_policy), tb, blob_data(msg), blob_len(msg));
 
   if (tb[WWAN_IF_CONNECT_MSG])
     msgstr = blobmsg_data(tb[WWAN_IF_CONNECT_MSG]);
 
-  hreq = calloc(1, sizeof(*hreq) + strlen(format) + strlen(obj->name) + strlen(msgstr) + 1);
+  hreq_size = sizeof(*hreq) + strlen(format) + strlen(obj->name) + strlen(msgstr) + 1;
+  hreq = calloc(1, hreq_size);
+  memset(hreq, 0, hreq_size);
+  memset(msgstr, 0, 1024);
 
   if(wwan_if_connect_do(msgstr) == FALSE)
   {
@@ -661,13 +673,17 @@ static int wwan_if_disconnect(struct ubus_context *ctx, struct ubus_object *obj,
   const char *format = "%s received a message: %s";
   char data[1024];
   char *msgstr = data;
+  int hreq_size;
 
   blobmsg_parse(wwan_if_disconnect_policy, ARRAY_SIZE(wwan_if_disconnect_policy), tb, blob_data(msg), blob_len(msg));
 
   if (tb[WWAN_IF_DISCONNECT_MSG])
     msgstr = blobmsg_data(tb[WWAN_IF_DISCONNECT_MSG]);
 
-  hreq = calloc(1, sizeof(*hreq) + strlen(format) + strlen(obj->name) + strlen(msgstr) + 1);
+  hreq_size = sizeof(*hreq) + strlen(format) + strlen(obj->name) + strlen(msgstr) + 1;
+  hreq = calloc(1, hreq_size);
+  memset(hreq, 0, hreq_size);
+  memset(msgstr, 0, 1024);
 
   if(wwan_if_disconnect_do(msgstr) == FALSE)
   {
@@ -774,13 +790,17 @@ static int wwan_if_getaddr(struct ubus_context *ctx, struct ubus_object *obj,
   const char *format = "%s received a message: %s";
   char data[1024];
   char *msgstr = data;
+  int hreq_size;
 
   blobmsg_parse(wwan_if_getaddr_policy, ARRAY_SIZE(wwan_if_getaddr_policy), tb, blob_data(msg), blob_len(msg));
 
   if (tb[WWAN_IF_GETADDR_MSG])
     msgstr = blobmsg_data(tb[WWAN_IF_GETADDR_MSG]);
 
-  hreq = calloc(1, sizeof(*hreq) + strlen(format) + strlen(obj->name) + strlen(msgstr) + 1);
+  hreq_size = sizeof(*hreq) + strlen(format) + strlen(obj->name) + strlen(msgstr) + 1;
+  hreq = calloc(1, hreq_size);
+  memset(hreq, 0, hreq_size);
+  memset(msgstr, 0, 1024);
 
   if(wwan_if_getaddr_do(msgstr) == FALSE)
   {
