7f27f8005eaedab787c18a854b44a0d1e66ee1c2
linhu <linhu.ying@u-blox.com>
2015-07-01 17:03:30 +0200

    add asychronize test

--- package/ublox/app/ublxubustest/ublx_ubus_client/src/client.c
+++ package/ublox/app/ublxubustest/ublx_ubus_client/src/client.c
@@ -40,6 +40,15 @@ static struct blob_buf b;
 static char *ubus_object = "test";
 static char *ubus_method = "hello";
 
+static void test_client_fd_cb(struct ubus_request *req, int fd)
+{
+  printf("Got fd from the server, watching...\n");
+}
+
+static void test_client_complete_cb(struct ubus_request *req, int ret)
+{
+  printf("completed request, ret: %d\n", ret);
+}
 
 static void receive_call_result(struct ubus_request *req, int type, struct blob_attr *msg)
 {
@@ -54,14 +63,37 @@ static void receive_call_result(struct ubus_request *req, int type, struct blob_
   free(str);
 }
 
+void ubus_sync_call(char *msg, uint32_t id)
+{
+  /* init message buffer */
+  blob_buf_init(&b, 0);
+
+  /* invoke a ubus call */
+  ubus_invoke(ctx, id, ubus_method, b.head, receive_call_result, 0, 3000);
+
+}
+
+void ubus_async_call(char *msg, uint32_t id)
+{
+  static struct ubus_request req;
+
+  blob_buf_init(&b, 0);
+  blobmsg_add_string(&b, "msg", "blah");
+  ubus_invoke_async(ctx, id, ubus_method, b.head, &req);
+  req.fd_cb = test_client_fd_cb;
+  req.complete_cb = test_client_complete_cb;
+  ubus_complete_request_async(ctx, &req);
+  uloop_timeout_set(&count_timer, 2000);
+}
+
 
 int main()
 {
   static struct ubus_request req;
   uint32_t id;
-  int ret, ret_ubus_invoke;
+  int ret;
   const char *ubus_socket = NULL;
-  char *msg = NULL;
+  char *msg = "hello";
 
   /* send a request with ubusd. */
   ctx = ubus_connect(ubus_socket);
@@ -76,19 +108,11 @@ int main()
     exit(1);
   }
 
-  /* init message buffer */
-  blob_buf_init(&b, 0);
-    
-  if(msg != NULL)
-  {
-    if(!blobmsg_add_json_from_string(&b, msg)) {
-      printf("Failed to parse message data\n");
-      return -1;
-    }
-  }
+  ubus_sync_call(msg, id);
 
-  /* invoke a ubus call */
-  ret_ubus_invoke = ubus_invoke(ctx, id, ubus_method, b.head, receive_call_result, 0, 3000);
+  sleep(10);
+
+  ubus_async_call(msg, id);
 
   /* free ubus structure */
   ubus_free(ctx);
